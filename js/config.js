07427fe151f3d9e305ac73dc1ef3a946a84bc18a
costrows0126 <58047246+costrows0126@users.noreply.github.com>
2025-12-02 14:40:51 -0700

    Add userPoolClientSecret to config
    Added userPoolClientSecret to the configuration.

--- js/config.js
+++ js/config.js
@@ -2,10 +2,12 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_bht7Lhhuz', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '49asn93kecutu7og6ce4chku14', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: '1f1ab7pt7hcf050vtt08eg3q3j4po5uhca45bn9rtlhnhbgsus98',
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
