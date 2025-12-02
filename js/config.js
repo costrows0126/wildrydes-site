aa6d342205e4da7ccf28b633dbe6f1bdd846a71c
costrows0126 <58047246+costrows0126@users.noreply.github.com>
2025-12-02 15:09:11 -0700

    Change userPoolClientId in config.js
    Updated userPoolClientId in the configuration.

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_bht7Lhhuz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '49asn93kecutu7og6ce4chku14', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        SECRET_HASH: '1f1ab7pt7hcf050vtt08eg3q3j4po5uhca45bn9rtlhnhbgsus98',
+        userPoolClientId: '5uqusmc2mmekc9t38dmjeemb8s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
